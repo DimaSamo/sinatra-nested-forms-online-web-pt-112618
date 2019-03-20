fdce82f571b0f479cd32115c562f6d757ea33476
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-20 01:02:36 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+  def initialize(attribute_hash)
+    attribute_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all.push(self)
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(attribute_hash)
+    attribute_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all=[]
+  end
 end

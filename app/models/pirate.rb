13e278a2aefebc68c04ff172c9a355cc226ee793
Dmitri Samotessov <dimasamo96@gmail.com>
2019-03-20 01:03:30 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,9 +2,9 @@ class Pirate
   attr_accessor :name, :weight, :height
   @@all = []
   def initialize(attribute_hash)
-    attribute_hash.each do |k, v|
-      self.send("#{k}=", v)
-    end
+    @name = attribute_hash[:name]
+    @weight = attribute_hash[:weight]
+    @height = attribute_hash[:height]
     @@all.push(self)
   end
 end
